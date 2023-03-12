2669ba5d6e4145ffdfead41c4c8a0edeb4256e21
Ashot.Ordukhanyan <orduka@gmail.com>
2023-03-11 20:58:09 -0500

    updates

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ from typing import List
 import logging
 import datetime
 from utils import df_file_cache
+import pandas_market_calendars as mcal
 
 @functools.lru_cache(maxsize=1)
 def getFinHubClient():
@@ -30,22 +31,9 @@ def getUniverse(name:str) -> List[str]:
 
 @functools.lru_cache(maxsize=5)
 @df_file_cache(cache_name='daily')
-def getDailyData(start:datetime.date,end:datetime.date,universe:str='REGULAR') -> pd.DataFrame:
+def getDailyData(start:datetime.date,end:datetime.date,universe:str='REGULAR', calendar = 'NYSE') -> pd.DataFrame:
     tickers = yf.Tickers(getUniverse(universe))
-    return tickers.history(interval='1d',start=start,end=end)
-#
-# def testIC():
-#     logging.basicConfig(level=logging.DEBUG)
-#     idxs = 'GSPC RUI RUT RUA'.split()
-#     constituents = {i:tuple(getIndexConstituents('^'+i)) for i in idxs}
-#     # for i,c in constituents.items():
-#     #     logging.info(f'{i} has {len(c)} constituents')
-#     for i in range(len(idxs)):
-#         for j in range(i+1,len(idxs)):
-#             ci = constituents[idxs[i]]
-#             cj = constituents[idxs[j]]
-#             logging.info(f"{idxs[i]}({len(ci)}) and {idxs[j]}({len(cj)}) have {len(set(ci)&set(cj))} common constituents")
-
-#if __name__ == '__main__':
-    #logging.basicConfig(level=logging.INFO)
-    #testIC()
+    cal = mcal.get_calendar(calendar)
+    daily = tickers.history(interval='1d',start=start,end=end)
+    valid_days = cal.valid_days(start,end).tz_localize(None)
+    return daily[daily.index.isin(valid_days)]
--- study.py
+++ study.py
@@ -9,11 +9,17 @@ def main():
     daily = data.getDailyData(start,end)
     logging.debug(f'Got daily data of shape{daily.shape}')
     logreturns = (1+((daily['Close'] - daily.shift(1)['Close']) / daily.shift(1)['Close'])).apply(np.log)
-    secs = daily['Close'].columns
-    closes = daily['Close']
-    secs = closes.columns
-    logreturns = closes.copy()
-    for sec in secs:
-        logreturns[sec]
+    #clean nas
+    shartShape = logreturns.shape
+    logreturns.dropna(axis=0, how='all', inplace=True)
+    if logreturns.shape != shartShape:
+        logging.warning(f'Dropping {shartShape[0]-logreturns.shape[0]} dates with NA values')
+        shartShape = logreturns.shape
+    logreturns.dropna(axis=1, how='any', inplace=True)
+    if logreturns.shape != shartShape:
+        logging.warning(f'Dropping {shartShape[1]-logreturns.shape[1]} instruments with NA values')
+        shartShape = logreturns.shape
+    #drop rows witl all nas
+
 if __name__ == '__main__':
     main()
